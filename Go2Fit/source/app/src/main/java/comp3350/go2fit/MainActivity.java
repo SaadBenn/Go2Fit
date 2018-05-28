27d9e1dbbe33cfe947fc9dba579a21c5a5f87113
Sean Davidson <Sean@wifi-wpa-cw2-140-193-69-173.wifi.umanitoba.ca>
2018-05-28 12:38:36 -0500

    Created Tabs and implemented track progress page

--- Go2Fit/source/app/src/main/java/comp3350/go2fit/MainActivity.java
+++ Go2Fit/source/app/src/main/java/comp3350/go2fit/MainActivity.java
@@ -1,16 +1,26 @@
 package comp3350.go2fit;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
+/**
+ * This app offers three view fragments and three tabs below the app bar
+ * to navigate to them, as well as the options menu showing Settings.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Creates the content view and toolbar, sets up the tab layout, and sets up a page adapter
+     * to manage views in fragments. The user clicks a tab and navigates to the view fragment.
+     *
+     * @param savedInstanceState Saved instance.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,35 +28,51 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        TabLayout layout = (TabLayout) findViewById(R.id.tab_layout);
+
+        layout.addTab(layout.newTab().setText(R.string.home));
+        layout.addTab(layout.newTab().setText(R.string.challenges));
+        layout.addTab(layout.newTab().setText(R.string.current_challenge));
+
+        layout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager tabs = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), layout.getTabCount());
+        tabs.setAdapter(adapter);
+        tabs.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(layout));
+        layout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onTabSelected(TabLayout.Tab tab) {
+                tabs.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            //nothing to see here...
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            //nothing to see here...
             }
         });
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    public boolean onCreateOptionsMenu(Menu menu_main) {
+        getMenuInflater().inflate(R.menu.menu_main, menu_main);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
---
+++ Go2Fit/source/app/src/main/java/comp3350/go2fit/TrackProgressService.java
@@ -0,0 +1,88 @@
+package comp3350.go2fit;
+
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.widget.TextView;
+
+public class TrackProgressService {
+    long previousTime;
+
+    public TrackProgressService()
+    {
+        previousTime = 0;
+    }
+    public TrackProgressModel getAccelerometer(SensorEvent event, TrackProgressModel current, int goalSteps) {
+        int numSteps = current.getNumSteps();
+
+        float[] values = event.values;
+        // Movement
+        float x = values[0];
+        float y = values[1];
+        float z = values[2];
+
+        float accelationSquareRoot = (x * x + y * y + z * z)
+                / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+        long actualTime = System.currentTimeMillis()/1000;;
+
+        if (accelationSquareRoot >= 2 && actualTime - previousTime > 0.5) //
+        {
+            numSteps++;
+            current.setNumSteps(numSteps);
+
+            double percentage = ((double) current.getNumSteps() / goalSteps);
+            current.setPercentageComplete((int)(percentage * 100));
+            previousTime = actualTime;
+        }
+
+        return current;
+    }
+
+    public String determineHours(long milliseconds)
+    {
+        String hours = Integer.toString((int)((milliseconds / (1000*60*60)) % 24));
+
+        if(hours.length() == 1)
+        {
+            hours = "0" + hours;
+        }
+        return hours;
+    }
+
+    public String determineMinutes(long milliseconds)
+    {
+        String minutes = Integer.toString((int)((milliseconds / (1000*60)) % 60));
+
+        if(minutes.length() == 1)
+        {
+            minutes = "0" + minutes;
+        }
+        return minutes;
+    }
+    public String determineSeconds(long milliseconds)
+    {
+        String seconds = Integer.toString((int)(milliseconds / 1000) % 60);
+
+        if(seconds.length() == 1)
+        {
+            seconds = "0" + seconds;
+        }
+        return seconds;
+    }
+
+    public double calculateDistance(TrackProgressModel model)
+    {
+        double distanceFeet = model.getNumSteps() * 2.3; //number of steps * 2.3 feet per step
+        double distanceMeters = distanceFeet * 0.3048; //convert the distance in feet to meters...simpler this way
+
+        model.setDistance(distanceMeters);
+        return distanceMeters;
+    }
+
+    public double calculateCaloriesBurned(TrackProgressModel model)
+    {
+        double mileComplete = model.getDistance() / 1609.34; //take the number of meters walked divded by number of meters in a mile
+        double calories = (model.getUserWeight() * 0.53) * mileComplete;
+
+        return calories;
+    }
+}
