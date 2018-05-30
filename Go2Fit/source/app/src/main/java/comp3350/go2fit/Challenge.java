86c1543b82b2f16db4b76a87cde36ec105441f4d
Eddie <umtorre8@myumanitoba.ca>
2018-05-30 09:27:21 -0500

    Eddie's list of challenges commit

--- Go2Fit/source/app/src/main/java/comp3350/go2fit/Challenge.java
+++ Go2Fit/source/app/src/main/java/comp3350/go2fit/Challenge.java
@@ -1,4 +1,36 @@
 package comp3350.go2fit;
 
-public class Challenge {
+import java.util.ArrayList;
+
+public class Challenge
+{
+    private String name;
+    private String time;
+    private String points;
+
+    public Challenge(String name, String time, String points)
+    {
+        this.name   = name;
+        this.time   = time;
+        this.points = points;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+    public String getTime()
+    {
+        return this.time;
+    }
+    public String getPoints()
+    {
+        return this.points;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "name: " + this.name + ", time: " + this.time + ", points: " + this.points;
+    }
 }
--- Go2Fit/source/app/src/main/java/comp3350/go2fit/ChallengesModel.java
+++ Go2Fit/source/app/src/main/java/comp3350/go2fit/ChallengesModel.java
@@ -1,4 +1,55 @@
 package comp3350.go2fit;
 
-public class ChallengesModel {
+public class ChallengesModel
+{
+    private String [] challenges;
+    private String [] timeForChallenges;
+    private String [] pointsForChallenges;
+
+    public ChallengesModel()
+    {
+        this.challenges = new String[]{"run 10 km", "100 squats", "77 push-ups", "120 sit-ups", "Run from U of M to Pembina", "bike 25 km"
+                ,"squat 5000 pounds", "sprint for 500 meters", "wrestle chuck norris", "Over 9000 chin-ups"};
+
+        this.timeForChallenges = new String[]{"1 hour", "15 minutes", "10 minutes", "15 minutes", "30 minutes", "1 hour"};
+
+        this. pointsForChallenges = new String[]{"40 points", "30 points", "25 points", "25 points", "30 points", "100 points"};
+    }
+
+    public String getChallenge(int position)
+    {
+        assert(position >= 0);
+        assert(position <= challenges.length);
+
+        return this.challenges[position];
+    }
+    public String getTimeOfChallenge(int position)
+    {
+        assert(position >= 0);
+        assert(position <= timeForChallenges.length);
+
+        return this.timeForChallenges[position];
+    }
+    public String getPointsOfChallenge(int position)
+    {
+        assert(position >= 0);
+        assert(position <= pointsForChallenges.length);
+
+        return this.pointsForChallenges[position];
+    }
+
+    public String[] getChallenges()
+    {
+        return this.challenges;
+    }
+
+    public String[] getTimes()
+    {
+        return this.timeForChallenges;
+    }
+
+    public String[] getPoints()
+    {
+        return this.pointsForChallenges;
+    }
 }
