b4244a3d54095673a45d34a8602b52e625728781
Saad <mushtasb@myumanitoba.ca>
2018-06-08 12:40:28 -0500

    Update UserManagerInterface.java

--- Go2Fit/source/app/src/main/java/comp3350/go2fit/BuisnessLayer/DatabaseManagers/UserManagerInterface.java
+++ Go2Fit/source/app/src/main/java/comp3350/go2fit/BuisnessLayer/DatabaseManagers/UserManagerInterface.java
@@ -4,5 +4,5 @@ import comp3350.go2fit.Models.UserModel;
 
 public interface UserManagerInterface {
     UserModel getUser(int userId);
-    void updateUser(UserModel user);
+    boolean updateUser(UserModel user);
 }
